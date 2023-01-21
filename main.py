d7f7fc1cf919f230fbcfed4634f19983e4513722
umair <umair.ahmad5966@gmail.com>
2023-01-21 15:28:27 +0500

    added get_data to fetch cali housing data from sklearn dataset

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from mlflow import log_metric, log_param, log_artifacts
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+
+def get_data():
+    # load the boston dataset
+    boston = datasets.fetch_california_housing(return_X_y=False)
+
+    # defining feature matrix(X) and response vector(y)
+    X = boston.data
+    y = boston.target
+    return X, y
 
 if __name__=="__main__":
      # Log a parameter (key-value pair)

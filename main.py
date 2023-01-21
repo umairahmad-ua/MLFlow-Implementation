7f782930582b26ed5941c6f2a5eeedfb4353efe8
umair <umair.ahmad5966@gmail.com>
2023-01-21 15:32:02 +0500

    added function calls

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from mlflow import log_metric, log_param, log_artifacts
 from sklearn.model_selection import train_test_split
 from sklearn import datasets
+from training import training, testing, dump_model
 
 def get_data():
     # load the boston dataset
@@ -12,8 +13,13 @@ def get_data():
     return X, y
 
 if __name__=="__main__":
-     # Log a parameter (key-value pair)
-    log_param("param1", 2)
+     # downloading data
+    X, y = get_data()
+    # splitting X and y into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4,
+                                                        random_state=1)
+    model = training(X_train, y_train)
+    coef_, intercept_, score = testing(model, X_test, y_test)
+    dump_model(model)
 
-    # Log a metric; metrics can be updated throughout the run
-    log_metric('Accuracy', 0.98)
+
